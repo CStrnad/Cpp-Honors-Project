ee7a98715aece58e20e483c8eb17de6d2a22f0bc
CStrnad <chrisstrnad@gmail.com>
2019-08-22 13:18:33 -0500

    Completed Requirements for Chapters 3-4.

---
+++ Honors_Project.cpp
@@ -0,0 +1,17 @@
+/*
+Program Name: Honors_Project.cpp
+Author: Christopher Strnad
+Purpose:
+
+
+
+*/
+#include <iostream>   //Inports iostream library, allowing input and outputs.
+using namespace std;
+
+int main()   //Main Function.
+{
+  cout << "Author Name:\t\t Christopher Strnad.\n";
+  cout << "Semester:\t\t Fall 2019.\n";
+  cout << "Project Description: \t Create a program that displays the author's aptitude in writing C++ code.\n";
+}
